a785def64c02c9c6b031f50f391b1e42468ea220
Rizki Sadewa <rizki.sadewa@pegadaian.co.id>
2025-07-27 12:26:13 +0700

    fixing bug : adding shebang in entrypoint.sh

--- ollama-deepseek/ollama-entrypoint.sh
+++ ollama-deepseek/ollama-entrypoint.sh
@@ -1,19 +1,16 @@
-# ./ollama-entrypoint.sh
+#!/bin/sh
 # clear ready flag
 rm -f /tmp/ready
 
 ollama serve &
 
-# start ollama, wait for it to serve
 echo "Starting Ollama..."
 until curl -s http://localhost:11434 > /dev/null; do
   sleep 1
 done
 
-# all the models to install
 MODELS="codellama:7b deepseek-coder:6.7b"
 
-# pull and install models, or skip if they're present
 for MODEL in $MODELS; do
   if ! ollama list | grep -q "$MODEL"; then
     echo "⚡️ Pulling model: $MODEL"
@@ -23,8 +20,6 @@ for MODEL in $MODELS; do
   fi
 done
 
-# set container as ready
 touch /tmp/ready
 
-# start nginx
 nginx -g "daemon off;"
